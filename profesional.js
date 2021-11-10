e762e8042597b25020766c4007c638f3326695b4
StefaniaBuri <stefania_0506@hotmail.com>
2021-11-10 17:25:35 +0100

    update

---
+++ profesional.js
@@ -0,0 +1,26 @@
+class Professional{
+
+    constructor( id, name, age, genre, weight, height, hairColor, eyeColor, race,
+         isRetired, nationality, oscarNumbers, profession){
+            this.id=id;
+            this.name=name;
+            this.age=age;
+            this.genre=genre;
+            this.weight=weight;
+            this.height=height;
+            this.hairColor=hairColor;
+            this.eyeColor=eyeColor;
+            this.race=race;
+            this.isRetired=isRetired;
+            this.nationality=nationality;
+            this.oscarNumbers=oscarNumbers;
+            this.profession=profession;
+        }
+}
+
+
+
+
+
+
+module.exports = {Professional}
